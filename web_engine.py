c00ee28bec9a51dae6a2226825429b134ba0836a
HAKANMAZI <hak122hak@gmail.com>
2020-10-23 11:38:04 +0300

    Add files via upload

---
+++ web_engine.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup, SoupStrainer
+import requests
+
+url = "https://www.sondakika.com/"
+kelime = "PKK"
+
+def webara(url, kelime):
+    page = requests.get(url)    
+    data = page.text
+    soup = BeautifulSoup(data)
+
+    url_list =[]
+    for link in soup.find_all('a'):
+        ekle = link.get('href')[1:]
+
+        url2 = url+ekle 
+        res = requests.get(url2)
+        html_page = res.content
+        soup = BeautifulSoup(html_page, 'html.parser')
+
+        for i in soup.find_all('p'):
+            if kelime in i.get_text():
+                url_list.append(url2)
+
+    mylist = list(set(url_list))
+    return mylist
---
+++ web_ui.py
@@ -0,0 +1,52 @@
+import tkinter as tk 
+import web_engine as ara   
+import webbrowser
+
+root=tk.Tk() 
+root.geometry("1500x600") 
+
+link_var=tk.StringVar() 
+kelime_var=tk.StringVar() 
+  
+def submit(): 
+    link=link_entry.get() 
+    kelime=kelime_var.get() 
+
+    links = ara.webara(link, kelime)
+    for i in links:
+        link = tk.Label(root, text=i, fg="blue", cursor="hand2")
+        link.grid()
+        link.bind("<Button-1>", lambda event: webbrowser.open(link.cget("text")))
+      
+    print("The link is : " + str(link)) 
+    print("The kelime is : " + kelime) 
+      
+    link_var.set("") 
+    kelime_var.set("") 
+      
+      
+# creating a label for  
+# name using widget Label 
+link_label = tk.Label(root, text = 'Links: ', font=('calibre', 10, 'bold')) 
+# name using widget Entry 
+link_entry = tk.Entry(root, textvariable = link_var,font=('calibre',10,'normal')) 
+   
+# creating a label for password 
+kelime_label = tk.Label(root,text = 'Aradığın Kelime',font = ('calibre',10,'bold')) 
+# creating a entry for password 
+kelime_entry=tk.Entry(root, textvariable = kelime_var, font = ('calibre',10,'normal')) 
+   
+# creating a button using the widget  
+# Button that will call the submit function  
+sub_btn=tk.Button(root,text = 'Keşfet', command = submit) 
+   
+
+link_label.grid(row=0,column=0) 
+link_entry.grid(row=0,column=1) 
+kelime_label.grid(row=1,column=0) 
+kelime_entry.grid(row=1,column=1) 
+sub_btn.grid(row=2,column=1) 
+   
+# performing an infinite loop  
+# for the window to display 
+root.mainloop()
